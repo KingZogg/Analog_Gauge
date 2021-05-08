288e8d7bbfa79fb7c295316a575225c7c74d2291
KingZogg <fatmailbox@gmail.com>
2021-05-08 18:49:01 +0800

    Removed some states. Still working

--- Analog_Gauge.ino
+++ Analog_Gauge.ino
@@ -1,23 +1,37 @@
 /*
-  Dekatron Analog Gauge 2 Example - with Timer IRQ
+dekatronStep Dek1(1, 0, 12, 13, 11, true, 0);
+dekatronStep Dek2(2, 0, 9, 10, 8, true, 0);
+dekatronStep Dek3(3, 0, 6, 7, 5, true, 0);
+dekatronStep Dek4(4, 0, 3, 4, 2, true, 0);
+dekatronStep Dek5(5, 0, 30, 32, 28, true, 0);
+dekatronStep Dek6(6, 0, 26, 24, 22, true, 0);
+dekatronStep Dek7(7, 0, 25, 23, 27, true, 0);
+dekatronStep Dek8(8, 0, 29, 31, 33, true, 0); // fault in hardware
+dekatronStep Dek9(9, 0, 35, 39, 37, true, 0);
+dekatronStep Dek10(10, 0, 41, 45, 43, true, 0);
 
-  modified April 7, 2017
-  by Michael Moorrees
+//not connected
+dekatronStep Dek11(11, 0, 40, 42, 44, true, 0);
+dekatronStep Dek12(12, 0, 34, 38, 36, true, 0);
+dekatronStep Dek13(13, 0, 69, 68, 67, true, 0);
+dekatronStep Dek14(14, 0, 66, 65, 64, true, 0);
+dekatronStep Dek15(15, 0, 63, 62, 61, true, 0);
+
  */
 #include <TimerOne.h>
 
-int Scnt = 67;        // step count
+int stepCount = 67;        // step count
 int state = 0;        // State - Machine State
-int dk_ct = 0;        // Dekatron Guide count - 0,1, or 2
+int stepState = 0;        // Dekatron Guide count - 0,1, or 2
 int LED = 13;         // Test LED
-int Guide1 = 28;       // Guide 1 - G1 pin of 2-guide Dekatron
-int Guide2 = 26;       // Guide 2 - G2 pin of 2-guide Dekatron
-int Index = 24;       // Index   - NDX input pin. High when glow at K0
-int Ndx = 0;          // K0 index indicator variable
+int Guide1 = 6;       // Guide 1 - G1 pin of 2-guide Dekatron
+int Guide2 = 7;       // Guide 2 - G2 pin of 2-guide Dekatron
+int indexPin = 5;       // indexPin   - index input pin. High when glow at K0
+int index = 0;          // K0 indexPin indicator variable
 int Tick = false;     // 500uS tick
-int Icnt = 250;       // Idle counter
-int ct_10 = 20;       // 10mS counter
-int in_val = 0;       // ADC value scaled
+int idleCounter = 250;       // Idle counter
+int ten_mS_counter = 20;       // 10mS counter
+int analogInValue = 0;       // ADC value scaled
 //int sensorPin = A0;   // Analog input 0
 int sensorPin = 0;    // Analog input 0
 
@@ -29,21 +43,21 @@ void setup() {
 	Timer1.attachInterrupt(timerISR);
 	pinMode(Guide1, OUTPUT);
 	pinMode(Guide2, OUTPUT);
-	pinMode(Index, INPUT);
+	pinMode(indexPin, INPUT);
 	pinMode(LED, OUTPUT);
 
 	digitalWrite(LED, LOW);
-	Scnt = 67;
+	stepCount = 67;  //    preset 33mS
 	state = 0;
-	Ndx = 0;
-	D_adv();
+	index = 0;
+	go_Clockwise();
 }
 
 
-void d_step(int dk_bt) // Dekatron Step
+void d_step(int stepState) // Dekatron Step
 {
-	if (digitalRead(Index)) Ndx = true;   // Sample for glow at K0
-	switch (dk_bt) {
+	if (digitalRead(indexPin)) index = true;   // Sample for glow at K0
+	switch (stepState) {
 	case 0:                             // glow at a main cathode
 		digitalWrite(Guide1, LOW);
 		digitalWrite(Guide2, LOW);
@@ -58,119 +72,122 @@ void d_step(int dk_bt) // Dekatron Step
 	}
 }
 
-void D_adv()                    // Dekatron Advance - Clockwise
+void go_Clockwise()                    // Dekatron Advance - Clockwise
 {
-	dk_ct++;
-	if (dk_ct == 3) dk_ct = 0;
-	d_step(dk_ct);
+	stepState++;
+	if (stepState == 3) stepState = 0;
+	d_step(stepState);
 }
 
-void D_rev()                    // Dekatron Reverse - Counter-Clockwise
+void go_CounterClockwise()                    // Dekatron Reverse - Counter-Clockwise
 {
-	dk_ct--;
-	if (dk_ct == -1) dk_ct = 2;
-	d_step(dk_ct);
+	stepState--;
+	if (stepState == -1) stepState = 2;
+	d_step(stepState);
 }
 
 //
 // Idle - States (0 - 3)
 //
 
-void dk_action0() {             // Dekatron Action Routine 0 - Slow CW
-	if (Ndx) {                    //   When glow hits Ndx [K0] cathode, 
-		D_rev();                    //    step CCW
-		Ndx = false;                //    go to state 1
+void slowCW() {             // Dekatron Action Routine 0 - Slow CW
+	if (index) {                    //   When glow hits index [K0] cathode, 
+		go_CounterClockwise();                    //    step CCW
+		index = false;                //    go to state 1
 		state = 1;                  //
-		Scnt = 67;                  //    preset 33mS
+		stepCount = 67;                  //    preset 33mS
 	}
-	else {                        // Continue CW if not at Ndx
-		Scnt--;                     //
-		if (Scnt == 0) {              //
-			D_adv();                  //   CW one step every
-			Scnt = 67;                //   33mS
+	else {                        // Continue CW if not at index
+		stepCount--;                     //
+		if (stepCount == 0) {              //
+			go_Clockwise();                  //   CW one step every
+			stepCount = 67;                //   33mS
 		}
 	}
 }
 
-void dk_action1() {             // Dekatron Action Routine 1 - Coast CCW
-	Ndx = false;                  // 
-	Scnt--;                       // 
-	if (Scnt == 0) {                // After 33mS
-		D_rev();                    //  step once CCW
-		Ndx = false;                //  go to state 2
+/*
+void coastCCW() {             // Dekatron Action Routine 1 - Coast CCW
+	index = false;                  // 
+	stepCount--;                       // 
+	if (stepCount == 0) {                // After 33mS
+		go_CounterClockwise();                    //  step once CCW
+		index = false;                //  go to state 2
 		state = 2;                  //
-		Scnt = 66;                  //
+		stepCount = 66;                  //
 	}
 }
+*/
 
-void dk_action2() {             // Dekatron Action Routine 2 - Slow CCW
-	if (Ndx) {                    //   When glow hits Ndx [K0] cathode, 
-		D_adv();                    //    step CW
-		Ndx = false;                //    go to state 3
+void slowCCW() {             // Dekatron Action Routine 2 - Slow CCW
+	if (index) {                    //   When glow hits index [K0] cathode, 
+		go_Clockwise();                    //    step CW
+		index = false;                //    go to state 3
 		state = 3;                  //
-		Scnt = 67;                  //    preset 33mS
+		stepCount = 67;                  //    preset 33mS
 	}
-	else {                        // Continue CCW if not at Ndx
-		Scnt--;                     //
-		if (Scnt == 0) {              //
-			D_rev();                  //   CCW one step every
-			Scnt = 67;                //   33mS
+	else {                        // Continue CCW if not at index
+		stepCount--;                     //
+		if (stepCount == 0) {              //
+			go_CounterClockwise();                  //   CCW one step every
+			stepCount = 67;                //   33mS
 		}
 	}
 }
-
-void dk_action3() {             // Dekatron Action Routine 3 - Coast CW
-	Ndx = false;                  // 
-	Scnt--;                       // 
-	if (Scnt == 0) {                // After 33mS
-		D_adv();                    //  step once CCW
-		Ndx = false;                //  go to state 0
+/*
+void coastCW() {             // Dekatron Action Routine 3 - Coast CW
+	index = false;                  // 
+	stepCount--;                       // 
+	if (stepCount == 0) {                // After 33mS
+		go_Clockwise();                    //  step once CCW
+		index = false;                //  go to state 0
 		state = 0;                  //
-		Scnt = 66;                  //
+		stepCount = 66;                  //
 	}
 }
+*/
 
-void dk_action4() {             // Dekatron Action Routine 4 - Return 2
-	if (Ndx) {                    //   Return to Ndx, CCW
-		Ndx = false;                //     finally at Ndx, go to state 5
-		state = 5;                  //     set Scnt per ADC for excursion
-		Scnt = in_val;
-		if (Scnt == 0) Scnt = 1;
+void returnToIndexCCW() {             // Dekatron Action Routine 4 - Return 2
+	if (index) {                    //   Return to index, CCW
+		index = false;                //     finally at index, go to state 5
+		state = 5;                  //     set stepCount per ADC for excursion
+		stepCount = analogInValue;
+		if (stepCount == 0) stepCount = 1;
 	}
 	else {
-		D_rev();
+		go_CounterClockwise();
 	}
 }
 
-void dk_action5() {             // Dekatron Action Routine 5 - Gauge to Scnt CW
-	if (Scnt == 0) {                // 
-		Ndx = false;                // when Scnt dots out, go to state 4
+void gaugeToStepcount() {             // Dekatron Action Routine 5 - Gauge to stepCount CW
+	if (stepCount == 0) {                // 
+		index = false;                // when stepCount dots out, go to state 4
 		state = 4;                  //
 	}
-	else {                        // go Scnt dots CW
-		Scnt--;                     //
-		D_adv();                    //
+	else {                        // go stepCount dots CW
+		stepCount--;                     //
+		go_Clockwise();                    //
 	}
 }
 
 
-void I_tmr() {
-	if (in_val < 2) {
+void idleTimer() {
+	if (analogInValue < 2) {
 		//    digitalWrite(LED, LOW);
-		if (Icnt == 0) {
+		if (idleCounter == 0) {
 			if (state > 3) {
 				state = 0;
-				Scnt = 1;
+				stepCount = 1;
 			}
 		}
-		else Icnt--;
+		else idleCounter--;
 	}
 	else {
-		Icnt = 250;
+		idleCounter = 250;
 		//    digitalWrite(LED, HIGH);
 		if (state < 4) {
 			state = 4;
-			Scnt = 1;
+			stepCount = 1;
 		}
 	}
 }
@@ -185,33 +202,33 @@ void loop() {
 		Tick = false;
 		switch (state) {            // Do action per current state
 		case 0:
-			dk_action0();             // Idle: Slow_CCW
-			digitalWrite(LED, LOW);
+			slowCW();             // Idle: Slow_CCW
+		//	digitalWrite(LED, LOW);
 			break;
 		case 1:
-			dk_action1();             //       Coast_CCW
-			digitalWrite(LED, HIGH);
+		//	coastCCW();             //       Coast_CCW
+		//	digitalWrite(LED, HIGH);
 			break;
 		case 2:
-			dk_action2();             //       Slow_CW
-			digitalWrite(LED, LOW);
+			slowCCW();             //       Slow_CW
+		//	digitalWrite(LED, LOW);
 			break;
 		case 3:
-			dk_action3();             //       Coast_CW
-			digitalWrite(LED, HIGH);
+		//	coastCW();             //       Coast_CW
+		//	digitalWrite(LED, HIGH);
 			break;
 		case 4:
-			dk_action4();             // Gauge: Return 2
+			returnToIndexCCW();             // Gauge: Return 2
 			break;
 		case 5:
-			dk_action5();             //        Gauge_CW
+			gaugeToStepcount();             //        Gauge_CW
 		}
-		ct_10--;
-		if (ct_10 == 0) {               // Every 10mS: Read ADC
-			ct_10 = 20;
-			in_val = analogRead(sensorPin) / 35;  // 0-5V
-		 //   in_val = analogRead(sensorPin)/56;  // Microphone Option
-			I_tmr();                    // Idle Timer
+		ten_mS_counter--;
+		if (ten_mS_counter == 0) {               // Every 10mS: Read ADC
+			ten_mS_counter = 20;
+			analogInValue = analogRead(sensorPin) / 35;  // 0-5V
+		 //   analogInValue = analogRead(sensorPin)/56;  // Microphone Option
+			idleTimer();                    // Idle Timer
 		}
 	}
 }
